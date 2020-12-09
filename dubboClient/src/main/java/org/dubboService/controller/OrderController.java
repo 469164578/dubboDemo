5df1f56f1e0d0a7f5dfbac3571a2bdf07487a99e
try <469164578@qq.com>
2020-12-10 06:53:04 +0800

    整理公共service值dubboApi

--- dubboClient/src/main/java/org/dubboService/controller/OrderController.java
+++ dubboClient/src/main/java/org/dubboService/controller/OrderController.java
@@ -1,6 +1,5 @@
 package org.dubboService.controller;
 
-import com.alibaba.dubbo.config.annotation.Reference;
 import org.dubboService.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
